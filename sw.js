499c5f467093cd033329fb74b867365f8d29c986
soljoon.kim <glen4040@naver.com>
2026-08-17 18:37:42 +0900

    회차 / 교시 / 문제번호를 각각 배지로 표시
    분야는 카드 아래쪽 태그로 분리
    카드에서 바로 ☆ 즐겨찾기 가능
    즐겨찾기만 보기 추가
    최신순 / 오래된순 정렬 추가
    필터를 모바일 가로 스크롤 방식으로 변경
    검색창 모바일 UI 개선
    통계에 단순 건수뿐 아니라 %도 같이 표시
    다크모드를 기본 화면으로 설정
    iPhone Safe Area 대응 강화
    하단 메뉴의 기출을 기출문제로 변경
    상세화면도 긴 문제/답안이 절대 좌우로 넘치지 않게 수정
    이전 PWA 캐시가 남지 않도록 캐시 버전도 V1.2로 변경

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='comsi-v1';const FILES=['./','./index.html','./style.css','./app.js','./questions.js','./quiz.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='comsi-v12';
+const FILES=['./','./index.html','./style.css','./app.js','./questions.js','./quiz.js','./manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(e.request.method==='GET'&&res.ok){let copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return res;}).catch(()=>caches.match('./index.html')))));
